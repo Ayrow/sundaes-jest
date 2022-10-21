c44091a1a884804b4025daae91215864526cd42f
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-21 10:01:08 +0200

    replaced firevent with userevent

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 test('checkbox unchecked by default and button disabled', () => {
@@ -13,16 +14,18 @@ test('checkbox unchecked by default and button disabled', () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test('checking checkbox enables button and disable on second click', () => {
+test('checking checkbox enables button and disable on second click', async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
   const checkbox = screen.getByRole('checkbox', {
     name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
