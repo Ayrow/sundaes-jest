5a49c5569ea439c657cb423ee668cf0b51c5a40a
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-21 10:14:32 +0200

    testing the popover and setup the popover in summaryform

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -29,3 +29,21 @@ test('checking checkbox enables button and disable on second click', async () =>
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
+
+test('popover response to hover', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
