141b54e817c6ecb88dda893092020d0ceb758362
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-25 11:01:29 +0200

    update app.js

--- src/pages/confirmation/OrderConfirmation.jsx
+++ src/pages/confirmation/OrderConfirmation.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
-import { useOrderDetails } from '../../contexts/OrderDetails';
+import { useOrderDetails } from '../../context/OrderDetails';
 
 export default function OrderConfirmation({ setOrderPhase }) {
   const { resetOrder } = useOrderDetails();
