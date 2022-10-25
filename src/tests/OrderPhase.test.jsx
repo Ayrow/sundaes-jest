0e1a23cc912a1ecb702cb0f2870722ef443effd4
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-25 11:35:05 +0200

    updated summary form to change phase on submit

--- src/tests/OrderPhase.test.jsx
+++ src/tests/OrderPhase.test.jsx
@@ -20,13 +20,36 @@ test('order phases for happy path', async () => {
   await user.click(cherryTopping);
 
   // find and click order button
-  const orderBtn = screen.getByRole('button', { name: 'order' });
+  const orderBtn = await screen.findByRole('button', {
+    name: 'Order Sundae!',
+  });
+  await user.click(orderBtn);
 
   // check summary information based on order
+  const scoopsSummary = await screen.findByRole('heading', {
+    name: /Scoops: \$/i,
+  });
+  expect(scoopsSummary).toHaveTextContent('4.00');
+
+  const toppingsSummary = await screen.findByRole('heading', {
+    name: /Toppings: \$/i,
+  });
+  expect(toppingsSummary).toHaveTextContent('1.50');
 
   // accept terms and conditions and click button to confirm order
+  const terms = await screen.findByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  await user.click(terms);
+
+  const confirmBtn = screen.getByRole('button', {
+    name: /confirm order/i,
+  });
+  await user.click(confirmBtn);
 
   // confirm order number on confirmation page
+  const orderNumber = await screen.findByText('123455676');
+  expect(orderNumber).toBeInTheDocument();
 
   // click new order button on confirmation page
 
