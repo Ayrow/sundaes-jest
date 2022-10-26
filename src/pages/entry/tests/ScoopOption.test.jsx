417d472f40718b4c9864b346a317c9c16462f49a
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-26 11:30:32 +0200

    wrote test to make sure the class is-invalid is used when invalid input for scoops

--- src/pages/entry/tests/ScoopOption.test.jsx
+++ src/pages/entry/tests/ScoopOption.test.jsx
@@ -10,4 +10,12 @@ test('when no scoop is added the input turns red', async () => {
   await user.clear(vanillaInput);
   await user.type(vanillaInput, '-3');
   expect(vanillaInput).toHaveClass('is-invalid');
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, '2.5');
+  expect(vanillaInput).toHaveClass('is-invalid');
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, '11');
+  expect(vanillaInput).toHaveClass('is-invalid');
 });
