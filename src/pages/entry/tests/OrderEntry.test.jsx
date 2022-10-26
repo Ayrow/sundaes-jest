940282e94eef57a47441f5c252572a9cd041b035
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-26 11:11:12 +0200

    wrote test to make sure order btn is disabled when no scoops are added

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -6,6 +6,7 @@ import {
 import OrderEntry from '../OrderEntry';
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
+import userEvent from '@testing-library/user-event';
 
 test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
@@ -23,4 +24,27 @@ test('handles error for scoops and toppings routes', async () => {
     const alerts = await screen.findAllByRole('alert');
     expect(alerts).toHaveLength(2);
   });
+});
+
+test('order button disabled if no scoops added', async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  const totalScoops = screen.getByText('Grand total: $', { exact: false });
+  expect(totalScoops).toHaveTextContent('0.00');
+
+  const orderBtn = screen.getByRole('button', { name: 'Order Sundae!' });
+  expect(orderBtn).toBeDisabled();
+
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, '3');
+  expect(orderBtn).toBeEnabled();
+  expect(totalScoops).toHaveTextContent('6.00');
+
+  await user.clear(vanillaInput);
+  expect(totalScoops).toHaveTextContent('0.00');
+  expect(orderBtn).toBeDisabled();
 });
