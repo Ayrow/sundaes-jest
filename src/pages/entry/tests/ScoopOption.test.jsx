41c772eb05a900ff7ac4dcbd56596db451ca7a3f
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-26 11:28:01 +0200

    updated scoopOption page to show red input when invalid value

---
+++ src/pages/entry/tests/ScoopOption.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from '../../../test-utils/testing-library-utils';
+import ScoopOptions from '../ScoopOption';
+import userEvent from '@testing-library/user-event';
+
+test('when no scoop is added the input turns red', async () => {
+  const user = userEvent.setup();
+  render(<ScoopOptions />);
+
+  const vanillaInput = screen.getByRole('spinbutton');
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, '-3');
+  expect(vanillaInput).toHaveClass('is-invalid');
+});
